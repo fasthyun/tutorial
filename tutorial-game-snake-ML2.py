3f5da927dc03fe1a6a5a9a295a180db068851d7b
hyun.dell3630 <hyun@dell3630>
2026-05-07 05:48:20 +0900

    update

--- tutorial-game-snake-ML2.py
+++ tutorial-game-snake-ML2.py
@@ -32,6 +32,7 @@ class SnakeEnv(SnakeGame):
     def reset(self):
         self.done = False
         self.steps =0 #!!!
+        self.reward = 0
         self.game_init()
         return self._get_state()
 
@@ -71,12 +72,13 @@ class SnakeEnv(SnakeGame):
         #if self.steps > 10 * (len(self.snake_body) + 3): # 무한 루프 방지
         #    self.reward -= 10
         #    self.done = True            
-        if self.step_count_without_reward > 30 : # no eat then, 
-            self.reward -= 1
+        if self.step_count_without_reward > 70 : # no eat then,             
+            self.reward -=1
             self.step_count_without_reward=0
-            if self.reward < -30 :      
-                self.done = True
-                self.reward = -100
+            self.done =True            
+            #if self.reward < -30 :
+            #    self.done = True
+            #    #self.reward = -100
         
         if self.score > self.score_prev: # hit something!
             self.reward += 10 #
@@ -112,6 +114,9 @@ class DQN(nn.Module):
 
 
 class DQNAgent1:
+    """
+    with epsilon
+    """
     def __init__(self,_policydict=None):
         
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -127,8 +132,7 @@ class DQNAgent1:
         
         self.epsilon = 1.0
         self.epsilon_min = 0.01
-        self.epsilon_decay = 0.996 
-        
+        self.epsilon_decay = 0.996
         self.batch_size = 50
         
     def remember(self, state, action, reward, next_state, done):
@@ -178,25 +182,33 @@ class DQNAgent1:
     def update_target(self):
         self.target_net.load_state_dict(self.policy_net.state_dict())
 
-class DQNAgent: # first make random than , make policy
+class DQNAgent: 
+    """
+        without epsilon!
+        
+        first make random than , make policy
+        
+    """
     def __init__(self,_policydict=None):       
-        self.policy_net = DQN(STATE_SIZE, ACTION_SPACE) # t실제 사용되는 신경망
-        self.target_net = DQN(STATE_SIZE, ACTION_SPACE)
+        self.policy_net = DQN(STATE_SIZE, ACTION_SPACE) # 실제 사용되는 신경망
+        self.target_net = DQN(STATE_SIZE, ACTION_SPACE) # 이건 ...
         self.target_net.load_state_dict(self.policy_net.state_dict())
         self.optimizer = optim.Adam(self.policy_net.parameters(), lr=1e-3)
         self.memory = deque(maxlen=20000)
-        self.gamma = 0.95
+        self.gamma = 0.95        
+        self.rand_count=0        
+        self.batch_size = 64
+        self.learn_count = 0
         
-        self.rand_count=0
-        
-        self.batch_size = 50
-        
-    def remember(self, state, action, reward, next_state, done):
+    def remember(self, state, action, reward, next_state, done):        
         self.memory.append((state, action, reward, next_state, done))
 
     def action(self, state):
-        if self.rand_count <= self.batch_size*500:
-            return random.randrange(ACTION_SPACE)
+        #if self.rand_count <= self.batch_size*10:
+        #    return random.randrange(ACTION_SPACE)
+        #    self.rand_count+=1
+        if self.learn_count < 10 :
+            return random.randrange(ACTION_SPACE)            
         
         state_t = torch.FloatTensor(state).unsqueeze(0)
         
@@ -206,17 +218,10 @@ class DQNAgent: # first make random than , make policy
     def learn(self): # learn
         if len(self.memory) < self.batch_size: 
             return
+        self.learn_count +=1
         batch = random.sample(self.memory, self.batch_size)
         states, actions, rewards, next_states, dones = map(np.array, zip(*batch))
-        #states, actions, rewards, next_states, dones = self.memory.sample(self.batch_size)
-        #states, actions, rewards, next_states, dones = zip(*batch)
-        """  
-            torch.tensor(np.array(states), dtype=torch.float32),
-            torch.tensor(np.array(actions), dtype=torch.long).unsqueeze(1),
-            torch.tensor(np.array(rewards), dtype=torch.float32).unsqueeze(1),
-            torch.tensor(np.array(next_states), dtype=torch.float32),
-            torch.tensor(np.array(dones), dtype=torch.float32).unsqueeze(1)
-        """
+        
         s_t = torch.FloatTensor(states)
         ns_t = torch.FloatTensor(next_states)
         a_t = torch.LongTensor(actions).unsqueeze(1)
@@ -232,9 +237,6 @@ class DQNAgent: # first make random than , make policy
         loss.backward()
         self.optimizer.step()
 
-        #if self.epsilon > self.epsilon_min:
-        #    self.epsilon_m = self.epsilon_decay
-
     def update_target(self):
         self.target_net.load_state_dict(self.policy_net.state_dict())
 
@@ -242,26 +244,23 @@ class DQNAgent: # first make random than , make policy
 # ─── TRAINING & INFERENCE ───────────────────────────────────────────────────────
 def train(epochs=1000, target_update=300):
     
-    env = SnakeEnv(render=False)  # 학습 시 렌더링 OFF (속도 향상)
+    env = SnakeEnv(render=True)  # 학습 시 렌더링 OFF (속도 향상)
     
     #agent = DQNAgent(torch.load("snake_dqn.pth"))
     agent = DQNAgent()
-    #agent.policy_net.load_state_dict(torch.load("snake_dqn_base1.pth", map_location=agent.device))
-    #agent.epsilon = 0.0  # Pure exploitation
-    
+    #agent.policy_net.load_state_dict(torch.load("snake_dqn_base1.pth"))#, map_location=agent.device))
+    #agent.epsilon = 0.0  # Pure exploitation  
     
     scores = []    
     #print(f"🚀 Training started on {agent.device}")
     for ep in range(1, epochs + 1):
-        state = env.reset() 
-        env.reward = 0
+        state = env.reset()         
         #print("x1")
         while True:
             _action = agent.action(state)
             next_state, reward, done, score = env.step(_action)
             agent.remember(state, _action, reward, next_state, done)            
-            state = next_state   
-            agent.rand_count+=1            
+            state = next_state            
             if done: 
                 break
             
@@ -271,7 +270,7 @@ def train(epochs=1000, target_update=300):
             agent.update_target()
         if ep % 50 == 0:
             avg = np.mean(scores[-50:])
-            print(f"Ep {ep:4d} | Eps: 0 | Avg50: {avg:.2f}", agent.rand_count)
+            print(f"Ep {ep:4d} | max: x | Avg50: {avg:.2f}", agent.rand_count)
             if avg > 28 :
                 break
             
@@ -281,13 +280,13 @@ def train(epochs=1000, target_update=300):
     
 
 def play():
-    model="snake_dqn_only1.pth"
+    model="snake_dqn_base1.pth"
     if not os.path.exists(model):
         print("❌ 학습된 모델이 없습니다. 먼저 train()을 실행하세요.")
         return
     env = SnakeEnv(render=True)
-    agent = DQNAgent()
-    agent.policy_net.load_state_dict(torch.load(model, map_location=agent.device))
+    agent = DQNAgent1()
+    agent.policy_net.load_state_dict(torch.load(model))#, map_location=agent.device))
     agent.epsilon = 0.0  # Pure exploitation
     
     print("🎮 Inference Mode - Press ESC to quit")
@@ -305,7 +304,7 @@ if __name__ == "__main__":
     
     mode = "train"
     if mode == "train":
-        train(epochs=2000)
+        train(epochs=5000)
     elif mode == "play":
         play()
     else:
